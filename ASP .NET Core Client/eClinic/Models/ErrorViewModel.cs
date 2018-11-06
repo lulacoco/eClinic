7512c1d67debcbb4e496b15d441c26ba6825da62
eternalamenity <eternalamenity@gmail.com>
2018-11-06 14:07:32 +0100

    adding a Visual Studio project

---
+++ ASP .NET Core Client/eClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eClinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
