4fe9cc57bbef0a4fc67085d57edea2d8f95fb2bc
eternalamenity <eternalamenity@gmail.com>
2018-11-11 19:46:10 +0100

    changed a form of a few tables

--- Database/create_tables.sql
+++ Database/create_tables.sql
@@ -4,20 +4,6 @@ GO
 USE Clinic
 GO
 
-CREATE TABLE DoctorLogin (
-	LoginID int IDENTITY(1,1) PRIMARY KEY,
-	Username varchar(100) NOT NULL,
-	Pass char(64) NOT NULL,
-	Email varchar(320) NOT NULL
-);
-
-CREATE TABLE PatientLogin (
-	LoginID int IDENTITY(1,1) PRIMARY KEY,
-	Username varchar(100) NOT NULL,
-	Pass char(64) NOT NULL,
-	Email varchar(320) NOT NULL
-);	 
-
 CREATE TABLE Diseases (
 	DiseaseID int IDENTITY(1,1) PRIMARY KEY,
 	DiseaseType varchar(100),
@@ -29,21 +15,36 @@ CREATE TABLE Doctor (
 	DoctorPesel int PRIMARY KEY,
 	FirstName varchar(50) NOT NULL,
 	LastName varchar(50) NOT NULL,
-	Age int,
-	LoginID int FOREIGN KEY REFERENCES DoctorLogin(LoginID)
+	Age int
+);
+
+CREATE TABLE DoctorLogin (
+	LoginID int IDENTITY(1,1) PRIMARY KEY,
+	Username varchar(100) NOT NULL,
+	Pass char(64) NOT NULL,
+	Email varchar(320) NOT NULL,
+	DoctorPesel int FOREIGN KEY REFERENCES Doctor(DoctorPesel)
 );
 
 CREATE TABLE Patient (
 	PatientPesel int PRIMARY KEY,
 	FirstName varchar(50) NOT NULL,
 	LastName varchar(50) NOT NULL,
-	Age int,
-	LoginID int FOREIGN KEY REFERENCES PatientLogin(LoginID),
+	Age int
 );
+
+CREATE TABLE PatientLogin (
+	LoginID int IDENTITY(1,1) PRIMARY KEY,
+	Username varchar(100) NOT NULL,
+	Pass char(64) NOT NULL,
+	Email varchar(320) NOT NULL,
+	PatientPesel int FOREIGN KEY REFERENCES Patient(PatientPesel)
+);	 
 
 CREATE TABLE Appointment (
 	AppointmentID int IDENTITY(1,1) PRIMARY KEY,
 	DoctorPesel int FOREIGN KEY REFERENCES Doctor(DoctorPesel),
 	PatientPesel int FOREIGN KEY REFERENCES Patient(PatientPesel),
-	DiseaseID int FOREIGN KEY REFERENCES Diseases(DiseaseID) 
+	DiseaseID int FOREIGN KEY REFERENCES Diseases(DiseaseID),
+	AppointmentDate DATETIME NOT NULL
 );
