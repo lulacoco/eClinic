c4ae424d6ae37bcddbdd5f460eb3e8bb1200e82d
eternalamenity <eternalamenity@gmail.com>
2018-11-21 00:22:14 +0100

    add similiar procedure as previous but for Patients appointments

--- Database/procedures.sql
+++ Database/procedures.sql
@@ -7,3 +7,12 @@ AS
 SELECT PatientPesel, AppointmentDate
 FROM Appointment
 WHERE DoctorPesel = @DoctorPesel AND AppointmentCompleted=0
+
+GO
+
+CREATE PROCEDURE GetPatientAppointments
+@PatientPesel bigint
+AS
+SELECT DoctorPesel, AppointmentDate
+FROM Appointment
+WHERE PatientPesel = @PatientPesel AND AppointmentCompleted=0
